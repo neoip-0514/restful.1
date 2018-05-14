e02414a152ce753a820c50a695a9e938b75f5136
neo <neoip.0514@gmail.com>
2018-05-14 16:04:11 +0900

    path variable 적용 #1 - e.g. /greeting/jhon

--- src/main/java/com/example/restful1/hello/GreetingController.java
+++ src/main/java/com/example/restful1/hello/GreetingController.java
@@ -2,6 +2,7 @@ package com.example.restful1.hello;
 
 import java.util.concurrent.atomic.AtomicLong;
 
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -15,6 +16,12 @@ public class GreetingController {
 
 	@RequestMapping(value = "/greeting", method = RequestMethod.GET)
 	public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
+		return new Greeting(counter.incrementAndGet(),
+				String.format(template, name));
+	}
+	
+	@RequestMapping(value = "/greeting/{name}", method = RequestMethod.GET)
+	public Greeting greeting2(@PathVariable(name="name") String name) {
 		return new Greeting(counter.incrementAndGet(),
 				String.format(template, name));
 	}
